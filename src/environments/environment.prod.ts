03592c00f976d35adf276e448cd3c5f5909f1733
jael.rivera@nordstrom.com <jael.rivera@nordstrom.com>
2023-10-22 14:58:44 -0500

    include https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  baseUrl:'http://34.171.225.63.nip.io/assignments',
-  urlPerfiles: 'http://34.171.225.63.nip.io/users/profiles' ,
-  urlPruebas: 'http://34.171.225.63.nip.io/assignments?status=in_progress&to_do' ,
-  urlUsuarios: 'http://34.171.225.63.nip.io/users' ,
-  urlResultados: 'http://34.171.225.63.nip.io/assignments?status=finished' ,
-  urlGuardarPregunta: 'http://34.171.225.63.nip.io/assignments/',
-  urlFinalizarprueba:'http://34.171.225.63.nip.io/assignments/questionnaire/'
+  baseUrl:'https://34.171.225.63.nip.io/assignments',
+  urlPerfiles: 'https://34.171.225.63.nip.io/users/profiles' ,
+  urlPruebas: 'https://34.171.225.63.nip.io/assignments?status=in_progress&to_do' ,
+  urlUsuarios: 'https://34.171.225.63.nip.io/users' ,
+  urlResultados: 'https://34.171.225.63.nip.io/assignments?status=finished' ,
+  urlGuardarPregunta: 'https://34.171.225.63.nip.io/assignments/',
+  urlFinalizarprueba:'https://34.171.225.63.nip.io/assignments/questionnaire/'
 };
