6431a1405b31d73d3a8e59fdf53a1f5b475083d6
jael.rivera@nordstrom.com <jael.rivera@nordstrom.com>
2023-10-23 20:23:37 -0500

    include https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   baseUrl:'https://34.171.225.63.nip.io/assignments',
   urlPerfiles: 'https://34.171.225.63.nip.io/users/profiles' ,
-  urlPruebas: 'https://34.171.225.63.nip.io/assignments?status=in_progress&to_do' ,
+  urlPruebas: 'https://34.171.225.63.nip.io/assignments?status=in_progress&status=to_do' ,
   urlUsuarios: 'https://34.171.225.63.nip.io/users' ,
   urlResultados: 'https://34.171.225.63.nip.io/assignments?status=finished' ,
   urlGuardarPregunta: 'https://34.171.225.63.nip.io/assignments/',
