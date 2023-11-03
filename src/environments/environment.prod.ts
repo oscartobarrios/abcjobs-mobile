01a40dc2d896503e767e69607b054726ac77eaaf
Oscar Tobar <o.tobar@uniandes.edu.co>
2023-11-03 16:24:06 -0500

    Prueba cambios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
+
 export const environment = {
-  production: true,
+  production: false,
   baseUrl:'https://34.171.225.63.nip.io/assignments',
   urlPerfiles: 'https://34.171.225.63.nip.io/users/profiles' ,
   urlPruebas: 'https://34.171.225.63.nip.io/assignments?status=in_progress&status=to_do' ,
@@ -7,8 +8,8 @@ export const environment = {
   urlResultados: 'https://34.171.225.63.nip.io/assignments?status=finished' ,
   urlGuardarPregunta: 'https://34.171.225.63.nip.io/assignments/',
   urlFinalizarprueba:'https://34.171.225.63.nip.io/assignments/questionnaire/',
-  urlPreentrevista: 'https://34.171.225.63.nip.io/interviews/offer/5683780991844352/pre',
-  urlEmpresas: 'http://34.171.225.63.nip.io/companies',
-  urlOfertas: 'http://34.171.225.63.nip.io/companies/offer',
+  urlPreentrevista: 'https://34.171.225.63.nip.io/interviews/offer/',
+  urlEmpresas: 'https://34.171.225.63.nip.io/companies',
+  urlOfertas: 'https://34.171.225.63.nip.io/companies/offer',
   urlEntrevistas: 'https://34.171.225.63.nip.io/interviews',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,8 +12,8 @@ export const environment = {
   urlGuardarPregunta: 'https://34.171.225.63.nip.io/assignments/',
   urlFinalizarprueba:'https://34.171.225.63.nip.io/assignments/questionnaire/',
   urlPreentrevista: 'https://34.171.225.63.nip.io/interviews/offer/',
-  urlEmpresas: 'http://34.171.225.63.nip.io/companies',
-  urlOfertas: 'http://34.171.225.63.nip.io/companies/offer',
+  urlEmpresas: 'https://34.171.225.63.nip.io/companies',
+  urlOfertas: 'https://34.171.225.63.nip.io/companies/offer',
   urlEntrevistas: 'https://34.171.225.63.nip.io/interviews',
 };
 
